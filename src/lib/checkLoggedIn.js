5ce9e6355bd8e3b6b55cec97fe49a932b1ecc0ea
연승은 <seungeun_yeon@tmax.co.kr>
2021-08-22 20:37:30 +0900

    add: first commit

---
+++ src/lib/checkLoggedIn.js
@@ -0,0 +1,11 @@
+import { check } from '../api/auth/auth.ctrl';
+
+const checkLoggedIn = (ctx, next) => {
+  if (!ctx.state.user) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  return next();
+};
+
+export default checkLoggedIn;
